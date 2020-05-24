ae8b899f827a1e4995b8a33acf24071ee141c5cc
Santiago Garcia <santiago.garcia@posteo.de>
2020-05-24 15:56:29 +0200

    Set up load balancing and high availability

--- aws.tf
+++ aws.tf
@@ -2,6 +2,121 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+resource "aws_vpc" "webserver" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    name = "aws-bootstrap"
+  }
+}
+
+resource "aws_subnet" "subnetAZ1" {
+  vpc_id                  = aws_vpc.webserver.id
+  availability_zone       = "eu-central-1a"
+  cidr_block              = "10.0.0.0/18"
+  map_public_ip_on_launch = true
+  tags = {
+    name = "aws-bootstrap"
+    az   = "eu-central-1a"
+  }
+}
+
+resource "aws_subnet" "subnetAZ2" {
+  vpc_id                  = aws_vpc.webserver.id
+  availability_zone       = "eu-central-1b"
+  cidr_block              = "10.0.64.0/18"
+  map_public_ip_on_launch = true
+  tags = {
+    name = "aws-bootstrap"
+    az   = "eu-central-1b"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.webserver.id
+  tags = {
+    name = "aws-bootstrap"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.webserver.id
+  tags = {
+    name = "aws-bootstrap"
+  }
+}
+
+resource "aws_route" "default_public_route" {
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.internet_gateway.id
+}
+
+resource "aws_route_table_association" "route_table_associationAZ1" {
+  route_table_id = aws_route_table.route_table.id
+  subnet_id      = aws_subnet.subnetAZ1.id
+}
+
+resource "aws_route_table_association" "route_table_associationAZ2" {
+  route_table_id = aws_route_table.route_table.id
+  subnet_id      = aws_subnet.subnetAZ2.id
+}
+
+resource "aws_lb" "loadbalancer" {
+  name               = "webserver-elb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.webserver_sg.id]
+  subnets            = [aws_subnet.subnetAZ1.id, aws_subnet.subnetAZ2.id]
+  tags = {
+    name = "aws-bootstrap"
+  }
+}
+
+resource "aws_lb_listener" "listener_80" {
+  load_balancer_arn = aws_lb.loadbalancer.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+resource "aws_lb_listener" "listener_8081" {
+  load_balancer_arn = aws_lb.loadbalancer.arn
+  port              = 8081
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  target_type = "instance"
+  port        = 8081
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.webserver.id
+  health_check {
+    enabled  = true
+    protocol = "HTTP"
+  }
+  tags = {
+    name = "aws-bootstrap"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "target_group_attachment1" {
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = aws_instance.instance.id
+}
+
+resource "aws_lb_target_group_attachment" "target_group_attachment2" {
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = aws_instance.instance2.id
+}
 
 resource "aws_launch_template" "webserver" {
   name          = "webserver"
@@ -13,7 +128,7 @@ resource "aws_launch_template" "webserver" {
     type = "aws-bootstrap-webserver"
   }
   #iam_instance_profile = aws_iam_instance_profile.aws-bootstrap-instance-profile.name
-  security_group_names = ["${aws_security_group.webserver_sg.name}"]
+  security_group_names = [aws_security_group.webserver_sg.name]
 }
 
 resource "aws_instance" "instance" {
@@ -23,7 +138,8 @@ resource "aws_instance" "instance" {
   user_data            = aws_launch_template.webserver.user_data
   tags                 = aws_launch_template.webserver.tags
   iam_instance_profile = aws_iam_instance_profile.aws-bootstrap-instance-profile.name
-  security_groups      = aws_launch_template.webserver.security_group_names
+  security_groups      = [aws_security_group.webserver_sg.id]
+  subnet_id            = aws_subnet.subnetAZ1.id
 }
 
 resource "aws_instance" "instance2" {
@@ -33,7 +149,8 @@ resource "aws_instance" "instance2" {
   user_data            = aws_launch_template.webserver.user_data
   tags                 = aws_launch_template.webserver.tags
   iam_instance_profile = aws_iam_instance_profile.aws-bootstrap-instance-profile.name
-  security_groups      = aws_launch_template.webserver.security_group_names
+  security_groups      = [aws_security_group.webserver_sg.id]
+  subnet_id            = aws_subnet.subnetAZ2.id
 }
 
 
@@ -44,7 +161,15 @@ resource "aws_iam_instance_profile" "aws-bootstrap-instance-profile" {
 
 
 resource "aws_security_group" "webserver_sg" {
-  name = "Allow access to webserver"
+  name   = "Allow access to webserver"
+  vpc_id = aws_vpc.webserver.id
+  ingress {
+    description = "Allow access to load balancer"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     description = "Allow access to webserver"
     from_port   = 8081
