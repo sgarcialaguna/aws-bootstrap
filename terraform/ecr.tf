3f5440ab80ac1cc9f30e1d7b5719fe165d24aafa
Santiago Garcia <santiago.garcia@posteo.de>
2020-05-29 17:16:20 +0200

    Create ECR repository

---
+++ terraform/ecr.tf
@@ -0,0 +1,3 @@
+resource "aws_ecr_repository" "aws_bootstrap" {
+  name = "aws_bootstrap"
+}
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -9,3 +9,18 @@ resource "aws_s3_bucket" "my-code-deploy-bucket" {
     }
   }
 }
+
+resource "aws_s3_bucket" "aws-bootstrap-scripts" {
+  acl           = "private"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
