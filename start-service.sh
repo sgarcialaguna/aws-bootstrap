665b55cade4a47c8f2bd9292291d71ff6966f2da
Santiago Garcia <santiago.garcia@posteo.de>
2020-06-03 11:16:27 +0200

    Fix bug in stop script

--- start-service.sh
+++ start-service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 docker login --username AWS --password $(aws ecr get-login-password --region eu-central-1) 061199822233.dkr.ecr.eu-central-1.amazonaws.com
 docker pull 061199822233.dkr.ecr.eu-central-1.amazonaws.com/aws_bootstrap:latest
-docker run -d -p 8081:8081 061199822233.dkr.ecr.eu-central-1.amazonaws.com/aws_bootstrap:latest
+docker run -d -p 8081:8081 --name aws_bootstrap 061199822233.dkr.ecr.eu-central-1.amazonaws.com/aws_bootstrap:latest
--- stop-service.sh
+++ stop-service.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -xe
-docker stop 061199822233.dkr.ecr.eu-central-1.amazonaws.com/aws_bootstrap:latest || true
+docker stop aws_bootstrap || true
