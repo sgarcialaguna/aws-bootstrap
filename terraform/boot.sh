1789a9917e7b8f7150ed8f236457ef4fb9aed068
Santiago Garcia <santiago.garcia@posteo.de>
2020-06-03 11:16:38 +0200

    Set up Cloudwatch agent

--- terraform/boot.sh
+++ terraform/boot.sh
@@ -5,6 +5,16 @@ exec > /tmp/userdata.log 2>&1
 # Update all packages
 yum -y update
 yum install -y ruby jq wget aws-cli
+
+# Install CodeDeploy agent
 wget https://aws-codedeploy-eu-central-1.s3.amazonaws.com/latest/install -O /home/ec2-user/install-codedeployagent.sh
 chmod +x /home/ec2-user/install-codedeployagent.sh
 /home/ec2-user/install-codedeployagent.sh auto
+
+# Install CloudWatch agent
+wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O /home/ec2-user/awslogs-agent-setup.py
+wget https://s3.amazonaws.com/aws-codedeploy-us-east-1/cloudwatch/codedeploy_logs.conf -O /home/ec2-user/codedeploy_logs.conf
+chmod +x /home/ec2-user/awslogs-agent-setup.py
+python /home/ec2-user/awslogs-agent-setup.py -n -r eu-central-1 -c s3://aws-codedeploy-us-east-1/cloudwatch/awslogs.conf
+cat /home/ec2-user/codedeploy_logs.conf >> /var/awslogs/etc/awslogs.conf
+service awslogs restart
